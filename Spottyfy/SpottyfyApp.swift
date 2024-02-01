1514a0808d904901d5195f10b5c0dccfd2fab325
alexlehuynh <alexlehuynh@gmail.com>
2024-01-31 20:57:15 -0500

    Initial Commit

---
+++ Spottyfy/SpottyfyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpottyfyApp.swift
+//  Spottyfy
+//
+//  Created by Alex Huynh on 1/31/24.
+//
+
+import SwiftUI
+
+@main
+struct SpottyfyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
